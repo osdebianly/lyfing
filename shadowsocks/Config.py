c5ca0c1d9bade92d7a58a4eee23ace39fc24e5f4
root <root@bigcf.cf>
2016-02-25 21:22:46 +0800

    use sqlite

---
+++ shadowsocks/Config.py
@@ -0,0 +1,12 @@
+#Config
+MYSQL_HOST = 'localhost'
+MYSQL_PORT = 3306
+MYSQL_USER = 'root'
+MYSQL_PASS = '123456'
+MYSQL_DB = 'shadowsocks'
+
+MANAGE_PASS = 'ss233333333'
+#if you want manage in other server you should set this value to global ip
+MANAGE_BIND_IP = '127.0.0.1'
+#make sure this port is idle
+MANAGE_PORT = 23333
